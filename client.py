28dd08d87336eaff32a24ba2743455212b0949c1
gptjddldi <hystom38@gmail.com>
2023-03-04 18:13:29 +0900

    feat: Add simple methods

--- client.py
+++ client.py
@@ -1,22 +1,39 @@
-
 import socket
 
 class Client:
-    def __init__(self):
-      self.port = 10002
-      self.host = "localhost"
+    def __init__(self, host="localhost", port=10002):
+      self.host = host
+      self.port = port
 
       self.socket = socket.socket()
       self.connect()
 
     def connect(self):
         self.socket.connect((self.host, self.port))
-        
+
     def ping(self):
-        self.socket.sendall("ping".encode())
-        recv = self.socket.recv(1024)
-        print(recv.decode())
+        self.send_recv("ping")
+      
+    def close(self):
+       self.socket.close()
+
+    def set(self, key, value):
+      self.send_recv(f"set {key} {value}")
+      
+    def get(self, key):
+      self.send_recv(f"get {key}")    
+    
+    def send_recv(self, cmd):
+      self.socket.sendall(cmd.encode())
+      recv = self.socket.recv(1024)
+      print(recv.decode())
+    
     
 if __name__ == '__main__':
   c = Client()
   c.ping()
+  c.set("hihi", "22")
+  c.get("hihi")
+  c.get("hi")
+  c.close()
+  print(123)
--- server.py
+++ server.py
@@ -1,31 +1,50 @@
 import threading
 import socket
 
+class ThreadedServer(object):
+    def __init__(self, host="localhost", port=10002):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket()
+        self.store = dict()
+        self.sock.bind((self.host, self.port))
+        
+    def listen(self):
+        self.sock.listen(5)
+        print(f"Server listening on {self.host}:{self.port}")
 
-def handle(conn, addr):
-    while True:
-        data = str(conn.recv(1024), 'ascii')
-        print(data)
-        cur_thread = threading.current_thread()
-        response = bytes("pong", 'ascii')
-        conn.sendall(response)
-        if data == 'bye':
-            conn.close()
-            break
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            threading.Thread(target=self.handle_client, args=(client, address)).start()
+            
+    def handle_client(self, client, address):
+        print(f"Client {address[0]}:{address[1]} connected.")
+
+        while True:
+            try:
+                data = client.recv(1024).decode()
+                if data:
+                    client.send(self.parse_data(data).encode())
+                else:
+                    raise socket.error("Client disconnected")
+            except:
+                print(f"Client {address[0]}:{address[1]} disconnected.")
+                client.close()
+                return
+
+    def parse_data(self, data):
+        if (data.split(" ")[0] == "set"):
+            k, v = data.split(" ")[1:]
+            self.store[k] = v
+            return "set"
+
+        elif (data.split(" ")[0] == "get"):
+            k = data.split(" ")[1]
+            return self.store.get(k, "None")
+        return "pong"
+
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 10002
-    server  = socket.socket()
-    server.bind((HOST, PORT))
-    
-    while True:
-        server.listen(3)
-        conn, addr = server.accept()
-        # Start a thread with the server -- that thread will then start one
-        # more thread for each request
-        server_thread = threading.Thread(target=handle, args=(conn, addr))
-        # Exit the server thread when the main thread terminates
-        server_thread.start()
-        print("Server loop running in thread:", server_thread.name)
-
-
+    server = ThreadedServer()
+    server.listen()
